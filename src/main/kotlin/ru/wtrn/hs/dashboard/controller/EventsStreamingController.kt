a2d239216b4bd5a1144237bbe43d50a267f3dabd
Dmitry Romanov <me@wtrn.ru>
2020-02-29 01:00:31 +0300

    WIP: Setup rsocket

--- src/main/kotlin/ru/wtrn/hs/dashboard/controller/EventsStreamingController.kt
+++ src/main/kotlin/ru/wtrn/hs/dashboard/controller/EventsStreamingController.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import mu.KotlinLogging
+import org.springframework.messaging.Message
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.web.bind.annotation.RestController
 import ru.wtrn.hs.dashboard.dto.FrontendEventDto
@@ -16,13 +17,16 @@ class EventsStreamingController(
     private val objectMapper: ObjectMapper
 ) {
     private val logger = KotlinLogging.logger { }
+
     @MessageMapping("events")
-    suspend fun requestEvents(): Flow<FrontendEventDto> = flow {
-        val event = FrontendEventDto(
-            timestamp =LocalDateTime.now(ZoneId.of("Europe/Moscow")).toString()
-        )
-        logger.info { "Emitting $event" }
-        emit(event)
-        delay(1000)
+    fun requestEvents(): Flow<FrontendEventDto> = flow {
+        while (true) {
+            val event = FrontendEventDto(
+                timestamp = LocalDateTime.now(ZoneId.of("Europe/Moscow")).toString()
+            )
+            logger.info { "Emitting $event" }
+            emit(event)
+            delay(1000)
+        }
     }
 }
