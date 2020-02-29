cda633af3cbc9ad1659485a915db4a23a228e3cb
Dmitry Romanov <me@wtrn.ru>
2020-02-29 21:17:28 +0300

    Use BroadcastChannel to integrate BudgetEventsController and BudgetStatusKafkaListener

--- src/main/kotlin/ru/wtrn/hs/dashboard/controller/BudgetEventsController.kt
+++ src/main/kotlin/ru/wtrn/hs/dashboard/controller/BudgetEventsController.kt
@@ -1,6 +1,8 @@
 package ru.wtrn.hs.dashboard.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import kotlinx.coroutines.channels.BroadcastChannel
+import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -9,22 +11,27 @@ import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.web.bind.annotation.RestController
 import ru.wtrn.hs.dashboard.dto.front.BudgetDto
 
+@Suppress("EXPERIMENTAL_API_USAGE")
 @RestController
 class BudgetEventsController(
     private val objectMapper: ObjectMapper
 ) {
     private val logger = KotlinLogging.logger { }
 
+    val channel = BroadcastChannel<BudgetDto>(1)
+
     @MessageMapping("events.budget")
     fun requestEvents(): Flow<BudgetDto> = flow {
-        while (true) {
-            val event = BudgetDto(
-                balance = "5 000",
-                today = "1 283",
-                tomorrow = "1 574"
-            )
-            emit(event)
-            delay(1000)
+        val intialEvent = BudgetDto(
+            balance = "0",
+            today = "0",
+            tomorrow = "0"
+        )
+//        emit(intialEvent)
+
+        channel.openSubscription().consumeEach {
+            logger.info { "Broadcasting $it" }
+            emit(it)
         }
     }
 }
--- src/main/kotlin/ru/wtrn/hs/dashboard/kafka/BudgetStatusKafkaListener.kt
+++ src/main/kotlin/ru/wtrn/hs/dashboard/kafka/BudgetStatusKafkaListener.kt
@@ -1,13 +1,18 @@
 package ru.wtrn.hs.dashboard.kafka
 
+import kotlinx.coroutines.channels.sendBlocking
 import mu.KotlinLogging
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Component
+import ru.wtrn.hs.dashboard.controller.BudgetEventsController
+import ru.wtrn.hs.dashboard.dto.front.BudgetDto
 import ru.wtrn.hs.dashboard.dto.kafka.BudgetStatusKafkaDto
 
 @Component
-class BudgetStatusKafkaListener {
+class BudgetStatusKafkaListener(
+    private val budgetEventsController: BudgetEventsController
+) {
     private val logger = KotlinLogging.logger { }
 
     @KafkaListener(topics = ["ru.wtrn.hs.dashboard.budget"])
@@ -15,5 +20,12 @@ class BudgetStatusKafkaListener {
         @Payload request: BudgetStatusKafkaDto
     ) {
         logger.info { request }
+        budgetEventsController.channel.sendBlocking(
+            BudgetDto(
+                balance = request.balance.toString(),
+                tomorrow = request.tomorrow.toString(),
+                today = request.today.toString()
+            )
+        )
     }
 }
