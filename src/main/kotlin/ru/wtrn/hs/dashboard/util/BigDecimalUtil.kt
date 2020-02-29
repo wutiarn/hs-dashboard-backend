a2b75f4f8de8b50b04dcac0d02397c12fc1a0173
Dmitry Romanov <me@wtrn.ru>
2020-02-29 23:38:06 +0300

    WIP: Decimal format

--- src/main/kotlin/ru/wtrn/hs/dashboard/util/BigDecimalUtil.kt
+++ src/main/kotlin/ru/wtrn/hs/dashboard/util/BigDecimalUtil.kt
@@ -1,12 +1,14 @@
 package ru.wtrn.hs.dashboard.util
 
 import java.math.BigDecimal
+import java.math.RoundingMode
 import java.text.DecimalFormat
 import java.text.DecimalFormatSymbols
 import java.util.Locale
 
-private val localizedFormat = DecimalFormat("#,###", DecimalFormatSymbols(Locale("ru")))
+private val localizedFormat = DecimalFormat.getInstance(Locale("ru"))
 
-fun BigDecimal.toLocalizedString(): String {
-    return localizedFormat.format(this)
+fun BigDecimal.toLocalizedString(scale: Int = 0, roundingMode: RoundingMode = RoundingMode.DOWN): String {
+    val scaled = this.setScale(scale, roundingMode)
+    return localizedFormat.format(scaled)
 }
